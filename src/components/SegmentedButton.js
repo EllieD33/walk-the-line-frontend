e501896870345093d74e1ab4871915f2d5be5adf
EllieD33 <120728802+EllieD33@users.noreply.github.com>
2024-07-21 16:34:10 +0100

    creates segmented button component

---
+++ src/components/SegmentedButton.js
@@ -0,0 +1,79 @@
+import { useState } from "react";
+import { View, StyleSheet, TouchableOpacity, Text } from "react-native";
+import globalStyles from "../styles/globalStyles";
+
+const SegmentedButton = ({ options, onSelect }) => {
+    const [selectedIndex, setSelectedIndex] = useState(null);
+
+    const handlePress = (index) => {
+        setSelectedIndex(index);
+        if (onSelect) {
+            onSelect(index);
+        }
+    };
+
+    return (
+        <View style={styles.container}>
+            {options.map((option, index) => (
+                <TouchableOpacity
+                    key={index}
+                    style={[
+                        styles.button,
+                        index === 0 && styles.firstButton,
+                        index === options.length - 1 && styles.lastButton,
+                        selectedIndex === index && styles.selectedButton,
+                    ]}
+                    onPress={() => handlePress(index)}
+                >
+                    <Text
+                        style={[
+                            globalStyles.textDark,
+                            selectedIndex === index &&
+                                styles.selectedButtonText,
+                        ]}
+                    >
+                        {option}
+                    </Text>
+                </TouchableOpacity>
+            ))}
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: "row",
+        borderRadius: 30,
+        overflow: "hidden",
+        backgroundColor: "#D4EADF",
+        borderWidth: 1,
+        borderColor: "#4AC483",
+        marginHorizontal: 20,
+        marginVertical: 5,
+    },
+    button: {
+        flex: 1,
+        paddingVertical: 10,
+        alignItems: "center",
+        justifyContent: "center",
+        borderRightWidth: 1,
+        borderColor: "#4AC483",
+    },
+    firstButton: {
+        borderTopLeftRadius: 4,
+        borderBottomLeftRadius: 4,
+    },
+    lastButton: {
+        borderTopRightRadius: 4,
+        borderBottomRightRadius: 4,
+        borderRightWidth: 0,
+    },
+    selectedButton: {
+        backgroundColor: "#4AC483", 
+    },
+    selectedButtonText: {
+        color: "#fff",
+    },
+});
+
+export default SegmentedButton;
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import { SafeAreaView, StyleSheet } from 'react-native';
 import FullMapView from '../components/FullMapView';
+import SegmentedButton from '../components/SegmentedButton';
 
 const HomeScreen = () => {
+
+    const onSelect = () => {
+
+    }
+
     return (
         <SafeAreaView style={styles.container}>
+            <SegmentedButton options={['Map view', 'List view']} onSelect={onSelect} />
             <FullMapView />
         </SafeAreaView>
     );
@@ -13,6 +20,8 @@ const HomeScreen = () => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        paddingTop: 40,
+        backgroundColor: '#D4EADF'
     },
 });
 
